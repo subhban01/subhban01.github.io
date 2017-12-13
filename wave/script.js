32878e75aefd7922c1019b4d134c896bcd2148eb
Subhadeep Banerjee <subhadeep.banerjee1@gmail.com>
2017-12-13 15:55:43 +0530

    snap to grid -- wave

--- wave/script.js
+++ wave/script.js
@@ -17,8 +17,10 @@ grd.addColorStop(0.5, "#dcd182");
 grd.addColorStop(0.75, "#bce695");
 grd.addColorStop(1, "#36f5b0");
 
-var textSpacing = 920/7;
+var textSpacing = 130;
 var sliderY = 310;
+var sliderStartX = 95, sliderEndX = 893;
+var sliderWidth = 798, dotPosition = sliderWidth/6;
 
 function init(){
 ctx.globalCompositeOperation='source-over';
@@ -29,7 +31,7 @@ ctx.globalCompositeOperation='destination-over';
 ctx.fillStyle = grd;
 ctx.fillRect(0,380, c.width, c.height);
 	//adding feedback options
-	var sideBuffer = 50;
+	var sideBuffer = 65;
 	ctx.font = "15px Arial";
 	ctx.fillStyle = 'black';
 	ctx.fillText("STRONGLY",sideBuffer,250);
@@ -45,39 +47,57 @@ ctx.fillRect(0,380, c.width, c.height);
 	ctx.fillText("AGREE",(textSpacing*6)+sideBuffer,272);
 	//slider bar
 	ctx.beginPath();
-	ctx.moveTo(sideBuffer+30, sliderY);
-	ctx.lineTo(920-50,sliderY);
+	ctx.moveTo(sliderStartX, sliderY);
+	ctx.lineTo(sliderEndX,sliderY);
 	ctx.lineWidth = 2;
 	ctx.stroke();
 	ctx.closePath();
 
 	for(i=0; i<7; i++){
 		ctx.beginPath();
-		ctx.arc((textSpacing*i)+80, sliderY, 3, 0, 2*Math.PI);
+		ctx.arc(sliderStartX+dotPosition*i, sliderY, 3, 0, 2*Math.PI);
 		ctx.closePath();
 		ctx.fill();
 	}
+
+	//other waves
+function smallerWaves(x,y,x1,y1,x2,y2){
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	ctx.globalCompositeOperation='destination-over';
+	ctx.quadraticCurveTo(x1, y1, x2, y2);
+	ctx.fillStyle = grd;
+	ctx.fill();
+	ctx.closePath();
+}
+
+smallerWaves(0, 400, 0, 320, 500, 400);
+smallerWaves(250, 400, 250, 320, 500, 400);
+smallerWaves(300, 400, 850, 320, 850, 400);
+smallerWaves(500, 400, 950, 300, 1020, 400);
+
+
 }
 
 init();
 //default bezier {x: 490, y: 300}
 	ctx.beginPath();
-	ctx.moveTo(280, 720);
+	ctx.moveTo(295, 720);
 	ctx.globalCompositeOperation='destination-over';
-	ctx.quadraticCurveTo(480,-165, 680, 720);
+	ctx.quadraticCurveTo(495,-165, 680, 720);
 	ctx.fillStyle = grd;
 	ctx.fill();
 	ctx.closePath();
 //default grabber
 	ctx.globalCompositeOperation='source-over';
 	ctx.beginPath();
-	ctx.arc((textSpacing*3.5)+20, sliderY, 20, 0, 2*Math.PI);
+	ctx.arc(sliderStartX+dotPosition*3, sliderY, 20, 0, 2*Math.PI);
 	ctx.fillStyle = '#fff';
 	ctx.closePath();
 	ctx.fill();
 //default triangle
 	ctx.beginPath();
-	var triangleX = (textSpacing*3.5)+10;
+	var triangleX = sliderStartX+(dotPosition*3)-10;
 	var triangleY = sliderY+5;
 	ctx.moveTo(triangleX,triangleY);
     ctx.lineTo(triangleX+10,triangleY-15);
@@ -86,32 +106,47 @@ init();
 	ctx.closePath();
 	ctx.fill();
 
+
+var pitStops = [];
+for(i=0; i<7; i++){
+	pitStops.push(sliderStartX+dotPosition*i);
+	}
+
 //onclick handler
 var x, y, startTime;
 c.addEventListener('click', function(evt){
 	var rect = c.getBoundingClientRect();
 	x = evt.clientX - rect.left, y = evt.clientY - rect.top;
 	// console.log(x,y);
-	// ctx.beginPath();
-	// ctx.moveTo(x-200, y+420);
-	// ctx.quadraticCurveTo(x,y-500, x+200, y+420);
-	// ctx.globalCompositeOperation='destination-over';
-	// ctx.fillStyle = grd;
-	// ctx.fill();
-	// ctx.closePath();
+	var tempX;
+	for(i=0; i<7; i++){
+		if(i == 0 && x < pitStops[i]+(dotPosition/2) && x >= 0){
+			tempX = pitStops[i];
+		}
+		else if(i == 6 && x <= 1000 && x > pitStops[i]-(dotPosition/2)){
+			tempX = pitStops[i];
+		}
+		else if(x < pitStops[i]+(dotPosition/2) && x > pitStops[i]-(dotPosition/2)){
+			tempX = pitStops[i];
+		}
+	}
+	x = tempX;
+
+	window.setTimeout(function(){
 	startTime = (new Date()).getTime();
-	if(x < oldX){
-		animate(oldX, startTime, true);
+	if(x < grabberPos){
+		animate(startTime, true);
 	}
 	else{
-		animate(oldX, startTime);
+		animate(startTime, false);
 	}
+},500);
 })
 function drawBeziers(x1){
-	if(x1 > (textSpacing*6)+80){
-		x1 = (textSpacing*6)+80;
+	if(x1 >= sliderStartX+dotPosition*6){
+		x1 = sliderStartX+dotPosition*6;
 	}
-	else if(x1 < 80){
+	else if(x1 <= 80){
 		x1 = 80;
 	}
 	if(y > 317){
@@ -120,7 +155,6 @@ function drawBeziers(x1){
 	else if(y < 280){
 		y = 280;
 	}
-	// console.log(x1,y);
 	ctx.beginPath();
 	ctx.moveTo(x1-200, y+420);
 	ctx.globalCompositeOperation='destination-over';
@@ -129,15 +163,14 @@ function drawBeziers(x1){
 	ctx.fill();
 	ctx.closePath();
 }
+var grabberPos = (sliderWidth/2) + sliderStartX;
 function drawGrabber(x){
-	oldX = x;
-	if(x > (textSpacing*6)+80){
-		x = (textSpacing*6)+80;
+	if(x >= sliderStartX+dotPosition*6){
+		x = sliderStartX+dotPosition*6;
 	}
-	else if(x < 80){
+	else if(x <= 80){
 		x = 80;
 	}
-
 	//place grubber on selected points. [0-147] interval is 130
 	ctx.globalCompositeOperation='source-over';
 	ctx.beginPath();
@@ -155,6 +188,8 @@ function drawGrabber(x){
 	ctx.fillStyle = '#000';
 	ctx.closePath();
 	ctx.fill();
+
+	grabberPos = x;
 }
 
 var clear = function(){
@@ -174,31 +209,31 @@ window.requestAnimFrame = (function(callback) {
 	};
 })();
 
+var pitStops = [];
+for(i=0; i<7; i++){
+	pitStops.push(sliderStartX+dotPosition*i);
+	}
+
+
+
 var oldX = 475;
 var duration = 490;
-function animate(x1, t, reverse) {
+
+function animate(t, reverse) {
+	var dist = Math.abs(grabberPos - x);
 	var time = (new Date()).getTime() - t;
-	// temp = prevX & prevX;
-	// console.log('t', time);
-	// console.log('x1 old',x1);
-	var linearSpeed = 250;
-        // pixels / second
-        var newX = linearSpeed * time/1000;
-        var x2 = x1;
-        // for(i = 0; i<100; i++){
-        	// newX++;
-        	if(newX < c.width) {
-        		// console.log('oldX',oldX, x2, time);
+	var linearSpeed = 100;
+        var newX = dist*linearSpeed/ duration;
+        // console.log(newX);
+        var x2 = grabberPos;
         		if(reverse){
         			x2 -= newX;
         		}
         		else{
         			x2 += newX;
         		}
-        	}
-        	// console.log(x2);
-        	if(time >= 500){
-				oldX = x2;
+        	if(time >= duration){
+					oldX = x2;
 			}
         	clear();
         	drawBeziers(x2);
@@ -206,7 +241,7 @@ function animate(x1, t, reverse) {
         // request new frame
         if(time < duration){
         	requestAnimFrame(function() {
-        		animate(x1, startTime, reverse);
+        		animate(startTime, reverse);
         	});
         }
     }
